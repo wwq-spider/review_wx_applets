dafd5ad86bccc6241d48880095f26ef1f0a1fe37
heyq-666 <854180667@qq.com>
2022-07-15 15:45:34 +0800

    专家咨询

--- main.js
+++ main.js
@@ -26,24 +26,62 @@ Vue.prototype.$apis = $apis;
 Vue.prototype.$store = store;
 Vue.prototype.$config = config;
 Vue.prototype.$footFontSize = fontSize;
-
-Vue.prototype.NumberSub = function(arg1, arg2) {
-    var re1, re2, m, n;
-    try {
-        re1 = arg1.toString().split(".")[1].length;
-    } catch (e) {
-    re1 = 0;
-    }
-    try {
-        re2 = arg2.toString().split(".")[1].length;
-    } catch (e) {
-        re2 = 0;
-    }
-    m = Math.pow(10, Math.max(re1, re2)); 
-    n = (re1 >= re2) ? re1 : re2;
-    return ((arg1 * m - arg2 * m) / m).toFixed(n);
-}
 // 全局字体配置结束
+
+Vue.mixin({
+	methods: {
+		setData: function(obj, callback) {
+			let that = this;
+			const handleData = (tepData, tepKey, afterKey) => {
+				tepKey = tepKey.split('.');
+				tepKey.forEach(item => {
+					if (tepData[item] === null || tepData[item] === undefined) {
+						let reg = /^[0-9]+$/;
+						tepData[item] = reg.test(afterKey) ? [] : {};
+						tepData = tepData[item];
+					} else {
+						tepData = tepData[item];
+					}
+				});
+				return tepData;
+			};
+			const isFn = function(value) {
+				return typeof value == 'function' || false;
+			};
+			Object.keys(obj).forEach(function(key) {
+				let val = obj[key];
+				key = key.replace(/\]/g, '').replace(/\[/g, '.');
+				let front, after;
+				let index_after = key.lastIndexOf('.');
+				if (index_after != -1) {
+					after = key.slice(index_after + 1);
+					front = handleData(that, key.slice(0, index_after), after);
+				} else {
+					after = key;
+					front = that;
+				}
+				if (front.$data && front.$data[after] === undefined) {
+					Object.defineProperty(front, after, {
+						get() {
+							return front.$data[after];
+						},
+						set(newValue) {
+							front.$data[after] = newValue;
+							that.$forceUpdate();
+						},
+						enumerable: true,
+						configurable: true
+					});
+					front[after] = val;
+				} else {
+					that.$set(front, after, val);
+				}
+			});
+			that.$forceUpdate();
+			isFn(callback) && that.$nextTick(callback);
+		}
+	}
+});
 
 App.mpType = 'app'
 
