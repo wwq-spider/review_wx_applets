961f7ed667873e717065c49a053674383253a3ac
heyq-666 <854180667@qq.com>
2022-09-28 22:35:47 +0800

    栋梁生涯测评

---
+++ pages/pdf/filePreview.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: '',
+			}
+		},
+		onLoad(params){
+			if(params.url){
+				this.url = params.url
+			}
+			let links = decodeURIComponent(this.url);
+			console.log(`/hybrid/html/pdf.html?url=${links}`)
+			this.url = `/hybrid/html/pdf.html?url=${links}`;
+		}
+	}
+</script>
+
+<style>
+</style>
